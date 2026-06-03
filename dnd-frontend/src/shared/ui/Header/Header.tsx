f15ec1cfc3176575c35580e90587bd7b431150cf
Artem Skichko <artem.skichko0@gmail.com>
2026-06-03 15:51:36 +0200

    home page

--- dnd-frontend/src/shared/ui/Header/Header.tsx
+++ dnd-frontend/src/shared/ui/Header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.scss';
 
 export const Header = () => {
   return (
-    <header className="absolute top-0 left-0 right-0 z-10 bg-black/60">
+    <header className="fixed top-0 left-0 right-0 z-10 bg-black/60">
       <nav className="font-(family-name:--font-cinzel) text-white text-lg p-4">
         <ul className="flex gap-6 justify-center items-center uppercase tracking-wide text-extrabold">
           <li>
