7dcfc601b3da8f17a206b74e9110ae9736a794ae
Artem Skichko <artem.skichko0@gmail.com>
2026-06-08 23:39:39 +0200

    created routes

--- dnd-frontend/src/App.tsx
+++ dnd-frontend/src/App.tsx
@@ -1,28 +1,64 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { MainLayout } from './app/layouts/MainLayout';
-import { HomePage } from './pages/HomePage/HomePage';
-import { ClassesPage } from './pages/ClassesPage/ClassesPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MainLayout } from "./app/layouts/MainLayout";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { ClassesPage } from "./pages/ClassesPage/ClassesPage";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route 
-          path="/" 
-          element={
-            <MainLayout>
-              <HomePage />
-            </MainLayout>
-          } 
-        />
-        <Route 
-          path="/classes" 
-          element={
-            <MainLayout>
-              <ClassesPage />
-            </MainLayout>
-          } 
-        />
+        {/* MAIN LAYOUT використовує Outlet для вкладень */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+
+          {/* CHARACTER PAGE */}
+          <Route path="character" element={<></>}>
+            <Route path="species" element={<></>}>
+              <Route path=":id" element={<></>} />
+            </Route>
+            <Route path="classes" element={<ClassesPage />}>
+              <Route path=":id" element={<></>} />
+            </Route>
+            <Route path="skills" element={<></>}>
+              <Route path=":id" element={<></>} />
+            </Route>
+          </Route>
+
+          {/* EQUIPMENT PAGE */}
+          <Route path="equipment" element={<></>}>
+            <Route path="weapons" element={<></>}>
+              <Route path=":id" element={<></>} />
+            </Route>
+            <Route path="armors" element={<></>}>
+              <Route path=":id" element={<></>} />
+            </Route>
+            <Route path="gear" element={<></>}>
+              <Route path=":id" element={<></>} />
+            </Route>
+          </Route>
+
+          {/* MAGIC PAGE */}
+          <Route path="magic" element={<></>}>
+            <Route path="spells" element={<></>}>
+              <Route path=":id" element={<></>} />
+            </Route>
+            <Route path="schools" element={<></>}>
+              <Route path=":id" element={<></>} />
+            </Route>
+          </Route>
+
+          {/* BESTIARY PAGE */}
+          <Route path="bestiary" element={<></>}>
+            <Route path="monster" element={<></>}>
+              <Route path=":id" element={<></>} />
+            </Route>
+          </Route>
+
+          {/* OTHER PAGES */}
+          <Route path="favorites" element={<></>} />
+          <Route path="account" element={<></>} />
+          <Route path="*" element={<></>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- dnd-frontend/src/app/layouts/MainLayout.tsx
+++ dnd-frontend/src/app/layouts/MainLayout.tsx
@@ -1,15 +1,14 @@
-import type { ReactNode } from "react";
 import { Header } from "../../shared/ui/Header/Header";
+import { Outlet } from "react-router-dom";
 
-interface MainLayoutProps {
-  children: ReactNode;
-}
 
-export const MainLayout = ({ children }: MainLayoutProps) => {
+export const MainLayout = () => {
   return (
     <div className="h-screen flex flex-col">
       <Header />
-      <main className="flex-1 min-h-0">{children}</main>
+      <main className="flex-1 min-h-0">
+        <Outlet />
+      </main>
       {/* <Footer /> */}
     </div>
   );
